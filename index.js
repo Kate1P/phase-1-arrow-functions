21381f0003f63ce868221f16e9fe7c78ae7a08f6
Kate Popova <popova1ea@gmail.com>
2022-11-25 18:17:47 -0800

    commit tree 91c7f3cb1de6c15bfae63467582514892ec24a93

---
+++ index.js
@@ -0,0 +1,8 @@
+function divide (){
+   let parameter1 = 2000; 
+    let parameter2 = 100; 
+    return parameter1/parameter2;
+}
+
+const square = (parametr1) => parametr1*parametr1;
+const add = (parametr1, parametr2) => parametr1 + parametr2;
